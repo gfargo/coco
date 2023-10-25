84643b8601578c1aa757757015c6941b27b7ab97
Griffen Fargo <ghfargo@gmail.com>
2023-10-25 10:37:18 -0400

    Add `getDiffForCommit` function in `simple-git` library
    This commit introduces a new function `getDiffForCommit` in the `simple-git` library. This function fetches the diff for a given commit ID. The function takes in two parameters, the commit ID and an object containing `git` and `logger`. It returns a promise that resolves to the diff of the commit. If there's an error while fetching the diff, it throws an error with a message detailing the commit ID and the error message.

---
+++ src/lib/simple-git/getDiffFromCommmit.ts
@@ -0,0 +1,23 @@
+import { DiffResult, SimpleGit } from "simple-git"
+import { Logger } from "../utils/logger"
+
+/**
+ * Fetches the diff for the given commit ID.
+ *
+ * @param commitId The commit ID for which the diff is to be retrieved.
+ * @returns A promise that resolves to the diff of the commit.
+ */
+export async function getDiffForCommit(commitId: string,  {
+  git,
+  logger,
+}: {
+  git: SimpleGit
+  logger?: Logger
+// }): Promise<DiffResult> {
+}): Promise<string> {
+  try {
+    return await git.diff(['-p', `${commitId}^..${commitId}`])
+  } catch (error) {
+    throw new Error(`Error fetching diff for commit ${commitId}: ${error.message}`)
+  }
+}
