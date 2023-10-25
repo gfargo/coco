00427c6d0ba35159caa4f0fdf90738e463b8ec88
Griffen Fargo <ghfargo@gmail.com>
2023-10-25 11:12:33 -0400

    Refactor `getDiffForCommit` function in `simple-git` library
    Removed unused `Logger` import and parameter from `getDiffForCommit` function in `simple-git` library. This simplifies the function signature and removes unnecessary dependencies. The function now only takes in a `commitId` and a `git` object of type `SimpleGit`. The function continues to return a promise that resolves to the diff of the commit.

--- src/lib/simple-git/getDiffFromCommmit.ts
+++ src/lib/simple-git/getDiffFromCommmit.ts
@@ -1,5 +1,4 @@
-import { DiffResult, SimpleGit } from "simple-git"
-import { Logger } from "../utils/logger"
+import { SimpleGit } from 'simple-git'
 
 /**
  * Fetches the diff for the given commit ID.
@@ -7,18 +6,17 @@ import { Logger } from "../utils/logger"
  * @param commitId The commit ID for which the diff is to be retrieved.
  * @returns A promise that resolves to the diff of the commit.
  */
-export async function getDiffForCommit(commitId: string,  {
-  git,
-  logger,
-}: {
-  git: SimpleGit
-  logger?: Logger
-// }): Promise<DiffResult> {
-}): Promise<string> {
+export async function getDiffForCommit(
+  commitId: string,
+  {
+    git,
+  }: {
+    git: SimpleGit
+  }
+): Promise<string> {
   try {
     return await git.diff(['-p', `${commitId}^..${commitId}`])
   } catch (error) {
     throw new Error(`Error fetching diff for commit ${commitId}: ${(error as Error).message}`)
   }
 }
-
