6835f161bce380ce016148cff69bcc6d9e8a6983
Griffen Fargo <ghfargo@gmail.com>
2023-11-15 11:10:41 -0500

    Refactor `getCommitLogRange` function in `simple-git` library
    This update refactors the `getCommitLogRange` function in the `simple-git` library. The function's parameters have been restructured to make the function more flexible. The `GetCommitLogRangeInput` type has been replaced with `GetCommitLogRangeOptions`, which now includes an optional `noMerges` property. This allows for the exclusion of merge commits. The function now also filters out any null values from the array. Error handling remains the same.

--- src/lib/simple-git/getCommitLogRange.ts
+++ src/lib/simple-git/getCommitLogRange.ts
@@ -1,28 +1,23 @@
 import { SimpleGit } from 'simple-git'
 
-export type GetCommitLogRangeInput = {
-  git: SimpleGit
-  from: string
-  to: string
-}
+type GetCommitLogRangeOptions = { git: SimpleGit; noMerges?: boolean }
 
-export async function getCommitLogRange({
-  git,
-  from,
-  to,
-}: GetCommitLogRangeInput): Promise<string[]> {
+export async function getCommitLogRange(
+  from: string,
+  to: string,
+  { noMerges, git }: GetCommitLogRangeOptions
+): Promise<string[]> {
   try {
-    // Using the 'raw' method to execute the 'git log' command with the desired options.
     const output = await git.raw([
       'log',
       `${from}..${to}`,
       '--pretty=format:%s', // This tells git to output only the commit messages.
-      // You could also include '--no-merges' here if you want to exclude merge commits.
-    ])
+      
+      // Include '--no-merges' here if you want to exclude merge commits.
+      noMerges ? '--no-merges' : null,
+    ].filter(Boolean) as string[]) // filter(Boolean) removes any null values from the array
 
-    // The 'output' will be a string with each commit message on a new line.
-    // Split the output by new lines to create an array of commit messages.
-    const messages = output.split('\n').filter(Boolean) // filter(Boolean) removes any empty strings
+    const messages = output.split('\n').filter(Boolean)
     return messages
   } catch (error) {
     // If there's an error, handle it appropriately
