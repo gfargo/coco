96b09dc500d243c8746737f32a0a5d3e394db385
Griffen Fargo <ghfargo@gmail.com>
2023-11-14 19:59:09 -0500

    Refactor `index.ts` to simplify `yargs` setup
    The `index.ts` file has been refactored to simplify the setup of `yargs`. The verbose setup has been replaced with a more concise usage method. The `commit` and `loadConfig` imports have been adjusted, and the `commit` command setup has been streamlined. A TODO comment has been added to address a type issue on the builder in the future.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 import yargs from 'yargs'
-import * as commit from './commands/commit'
-import { loadConfig } from './lib/config/loadConfig'
+import commit from './commands/commit'
 
-yargs
-  .scriptName('coco')
-  .commandDir('./commands', {
-    extensions: ['ts'],
-  })
-  .demandCommand()
-  .strict()
-  .option('h', { alias: 'help' })
-  .option('v', {
-    alias: 'verbose',
-    type: 'boolean',
-    description: 'Run with verbose logging',
-  }).argv
-
-export { commit, loadConfig }
+yargs.scriptName('coco').usage('$0 <cmd> [args]').command(
+  [commit.command, '$0'],
+  commit.desc,
+  // TODO: fix type on builder
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  commit.builder,
+  commit.handler
+).argv
