9284bd965b6d5fe0e50c5c3a3849a522b640fed2
Griffen Fargo <ghfargo@gmail.com>
2023-11-15 11:08:40 -0500

    Refactor functions to use declaration syntax
    Refactored the functions in `getDiff.ts`, `getStatus.ts`, and `getSummaryText.ts` to use function declaration syntax instead of arrow functions for consistency and readability. This includes `parseDefaultFileDiff`, `parseRenamedFileDiff`, `getDiff`, `getStatus`, and `getSummaryText`. Also, removed unnecessary semicolons and improved code formatting.

--- src/lib/simple-git/getStatus.ts
+++ src/lib/simple-git/getStatus.ts
@@ -1,36 +1,35 @@
 import { FileChangeStatus } from '../types'
 import { DiffResultBinaryFile, DiffResultTextFile, FileStatusResult } from 'simple-git'
 
-export const getStatus = (
+export function getStatus(
   file: FileStatusResult | DiffResultTextFile | DiffResultBinaryFile,
   location: 'index' | 'working_dir' = 'index'
-): FileChangeStatus => {
+): FileChangeStatus {
   if ('index' in file && 'working_dir' in file) {
-    const statusCode = file[location];
+    const statusCode = file[location]
 
     switch (statusCode) {
       case 'A':
-        return 'added';
+        return 'added'
       case 'D':
-        return 'deleted';
+        return 'deleted'
       case 'M':
-        return 'modified';
+        return 'modified'
       case 'R':
-        return 'renamed';
+        return 'renamed'
       case '?':
-        return 'untracked';
+        return 'untracked'
       default:
-        return 'unknown';
+        return 'unknown'
     }
   } else if ('changes' in file && 'binary' in file) {
-    if (file.changes === 0) return 'untracked';
-    if (file.file.includes('=>')) return 'renamed';
-    if (file.deletions === 0 && file.insertions > 0) return 'added';
-    if (file.insertions === 0 && file.deletions > 0) return 'deleted';
-    if ((file.insertions > 0 && file.deletions > 0) || file.changes > 0) return 'modified';
-    return 'unknown';
+    if (file.changes === 0) return 'untracked'
+    if (file.file.includes('=>')) return 'renamed'
+    if (file.deletions === 0 && file.insertions > 0) return 'added'
+    if (file.insertions === 0 && file.deletions > 0) return 'deleted'
+    if ((file.insertions > 0 && file.deletions > 0) || file.changes > 0) return 'modified'
+    return 'unknown'
   } else {
-    throw new Error("Invalid file type");
+    throw new Error('Invalid file type')
   }
 }
-
--- src/lib/simple-git/getSummaryText.ts
+++ src/lib/simple-git/getSummaryText.ts
@@ -2,20 +2,20 @@ import { DiffResultBinaryFile, DiffResultTextFile, FileStatusResult } from 'simp
 import { getStatus } from './getStatus'
 import { FileChange } from '../types'
 
-export const getSummaryText = (
+export function getSummaryText(
   file: FileStatusResult | DiffResultTextFile | DiffResultBinaryFile,
   change: Partial<FileChange>
-) => {
+) {
   const status = change.status || getStatus(file)
-  
-  let filePath: string;
+
+  let filePath: string
 
   if ('path' in file) {
-    filePath = file.path;
+    filePath = file.path
   } else if ('file' in file) {
-    filePath = change?.filePath || file.file;
+    filePath = change?.filePath || file.file
   } else {
-    throw new Error("Invalid file type");
+    throw new Error('Invalid file type')
   }
 
   if (change.oldFilePath) {
