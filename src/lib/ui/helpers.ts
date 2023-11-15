bf40058c699b8eb988a74cf19f4241e8d8ae794b
Griffen Fargo <ghfargo@gmail.com>
2023-11-14 19:33:50 -0500

    Rename function and adjust import path
    - Renamed `getModelAPIKey` to `getApiKeyForModel` in `utils.ts` to improve clarity and readability. The new name is more descriptive and aligns better with its functionality.
    - Adjusted the import path in `helpers.ts` from `../../commands/commit` to `../../commands/commit/options` to reflect the recent directory structure changes. This ensures that the `CommitOptions` are correctly imported and the application functions as expected.

--- src/lib/ui/helpers.ts
+++ src/lib/ui/helpers.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk'
-import { CommitOptions } from '../../commands/commit'
+import { CommitOptions } from '../../commands/commit/options'
 
 export const isInteractive = (argv: CommitOptions) => {
   return argv?.mode === 'interactive' || argv.interactive
