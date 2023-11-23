9194438e753f46adc5836e36a975bf56f8c17200
Griffen Fargo <ghfargo@gmail.com>
2023-11-23 17:10:44 -0500

    Refactor `getCommitLogCurrentBranch` and `getCommitLogRange` functions
    This update includes a few changes to the `getCommitLogCurrentBranch` and `getCommitLogRange` functions.
    
    In `getCommitLogCurrentBranch`, we've replaced the direct git commands with calls to `getCurrentBranchName` and `getCommitLogRange` to improve readability and reusability. The function now also retrieves a more detailed commit log, including the commit message, date, body, and author name.
    
    In `getCommitLogRange`, we've replaced the raw git command with the `git.log` function from the `simple-git` library. This change simplifies the code and makes it easier to understand.
    
    We've also added a new function `getCurrentBranchName` that encapsulates the logic for retrieving the current branch name. This function is used in `getCommitLogCurrentBranch` to get the current branch name.

--- src/lib/simple-git/getCommitLogCurrentBranch.ts
+++ src/lib/simple-git/getCommitLogCurrentBranch.ts
@@ -1,5 +1,7 @@
 import { SimpleGit } from 'simple-git'
 import { Logger } from '../utils/logger'
+import { getCommitLogRange } from './getCommitLogRange'
+import { getCurrentBranchName } from './getCurrentBranchName'
 
 export type GetCommitLogCurrentBranch = {
   git: SimpleGit
@@ -7,25 +9,36 @@ export type GetCommitLogCurrentBranch = {
   comparisonBranch?: string
 }
 
-export async function getCommitLogCurrentBranch({ git, logger, comparisonBranch = 'main' }: GetCommitLogCurrentBranch) : Promise<string[]> {
+export async function getCommitLogCurrentBranch({
+  git,
+  logger,
+  comparisonBranch = 'main',
+}: GetCommitLogCurrentBranch): Promise<string[]> {
   try {
     // Get the current branch name
-    const currentBranch = await git.revparse(['--abbrev-ref', 'HEAD']);
+    const branch = await getCurrentBranchName({ git })
 
     // Check if the current branch has any commits
-    const hasCommits = (await git.raw(['rev-list', '--count', currentBranch])) !== '0';
+    const hasCommits = (await git.raw(['rev-list', '--count', branch])) !== '0'
     if (!hasCommits) {
-      console.log('No commits on the current branch.');
-      return [];
+      console.log('No commits on the current branch.')
+      return []
     }
 
-    // Check for unique commits on the current branch
-    const uniqueCommits = await git.raw(['rev-list', `${comparisonBranch}..${currentBranch}`]);
+    // Get the list of commits that are unique to the current branch
+    const uniqueCommits = (await git.raw(['rev-list', `${comparisonBranch}..${branch}`]))
+      .split('\n')
+      .filter(Boolean)
+      .reverse()
 
-    return uniqueCommits.split('\n').filter(Boolean);
+    const firstCommit = uniqueCommits[0]
+    const lastCommit = uniqueCommits[uniqueCommits.length - 1]
+
+    // Retrieve commit log with messages
+    return await getCommitLogRange(firstCommit, lastCommit, { git, noMerges: true })
   } catch (error) {
     logger?.log('Encountered an error getting commit log from current branch', { color: 'red' })
   }
 
-  return [];
+  return []
 }
