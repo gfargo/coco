812736cfea8c04d3d442d658753420f17ab52e6d
Griffen Fargo <ghfargo@gmail.com>
2023-11-26 19:16:10 -0500

    Refactor yargs usage in `index.ts`
    Refactored the usage of `yargs` in `index.ts` to improve readability and maintainability. The `yargs` instance is now stored in a variable `y`, and the commands are chained off of it. This makes the code cleaner and easier to understand. The export statement has also been cleaned up for better readability.

--- src/index.ts
+++ src/index.ts
@@ -7,43 +7,40 @@ import init from './commands/init'
 import * as types from './lib/types'
 import { Config } from './lib/config/types'
 
-yargs
-  .scriptName('coco')
-  .usage('$0 <cmd> [args]')
-  .command(
-    [commit.command, '$0'],
-    commit.desc,
-    // TODO: fix type on builder
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    commit.builder,
-    commit.handler
-  )
-  .command(
-    changelog.command,
-    changelog.desc,
-    // TODO: fix type on builder
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    changelog.builder,
-    changelog.handler
-  )
-  .command(
-    init.command,
-    init.desc,
-    // TODO: fix type on builder
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    init.builder,
-    init.handler
-  )
-  .demandCommand()
-  .help().argv
+const y = yargs()
 
-export { 
-  changelog, 
-  commit, 
-  init,
-  types,
-  Config,
-}
+y.scriptName('coco').usage('$0 <cmd> [args]')
+
+y.command(
+  [commit.command, '$0'],
+  commit.desc,
+  // TODO: fix type on builder
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  commit.builder,
+  commit.handler
+)
+
+y.command(
+  changelog.command,
+  changelog.desc,
+  // TODO: fix type on builder
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  changelog.builder,
+  changelog.handler
+)
+
+y.command(
+  init.command,
+  init.desc,
+  // TODO: fix type on builder
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  init.builder,
+  init.handler
+)
+
+y.parse(process.argv.slice(2))
+
+export { changelog, commit, init, types, Config }
