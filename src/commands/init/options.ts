ebc2ad5dba86078cba965fb19062c972dfcd6df1
Griffen Fargo <ghfargo@gmail.com>
2023-11-28 13:40:43 -0500

    Refactor `init` command and clean up options
    This commit refactors the `init` command handler in `handler.ts` to simplify the code and improve readability. The changes include:
    
    - Removing unnecessary imports and adding new ones that are needed for the refactored code.
    - Replacing the `handleProjectLevelConfig` and `handleSystemLevelConfig` functions with inline code in the main `handler` function.
    - Changing the `handler` function to use the `CommandHandler` type and to take a logger as a parameter.
    - Adding a call to `checkAndHandlePackageInstallation` after the configuration is written.
    - Making minor changes to log messages for clarity and consistency.
    
    In `options.ts`, the `CommitOptions` interface and `options` object have been significantly simplified. The `CommitOptions` interface has been replaced with `InitOptions` and `InitArgv` types, and the `options` object now contains no options.
    
    Finally, in `index.ts`, the handler is now wrapped in a call to `commandExecutor` before being exported.

--- src/commands/init/options.ts
+++ src/commands/init/options.ts
@@ -1,64 +1,14 @@
 import { Options, Argv } from 'yargs'
-import { BaseCommandOptions } from '../types'
+import { BaseArgvOptions } from '../types'
 
-export interface CommitOptions extends BaseCommandOptions {
-  prompt: string
-  commit: boolean
-  summarizePrompt: string
-  openInEditor: boolean
-  ignoredFiles: string[]
-  ignoredExtensions: string[]
-}
+export type InitOptions = BaseArgvOptions
+
+export type InitArgv = Argv<InitOptions>['argv']
 
 /**
  * Command line options via yargs
  */
-export const options = {
-  model: { type: 'string', description: 'LLM/Model-Name' },
-  openAIApiKey: {
-    type: 'string',
-    description: 'OpenAI API Key',
-    conflicts: 'huggingFaceHubApiKey',
-  },
-  huggingFaceHubApiKey: {
-    type: 'string',
-    description: 'HuggingFace Hub API Key',
-    conflicts: 'openAIApiKey',
-  },
-  tokenLimit: { type: 'number', description: 'Token limit' },
-  prompt: {
-    type: 'string',
-    alias: 'p',
-    description: 'Commit message prompt',
-  },
-  i: {
-    type: 'boolean',
-    alias: 'interactive',
-    description: 'Toggle interactive mode',
-  },
-  s: {
-    type: 'boolean',
-    description: 'Automatically commit staged changes with generated commit message',
-    default: false,
-  },
-  e: {
-    type: 'boolean',
-    alias: 'edit',
-    description: 'Open commit message in editor before proceeding',
-  },
-  summarizePrompt: {
-    type: 'string',
-    description: 'Large file summary prompt',
-  },
-  ignoredFiles: {
-    type: 'array',
-    description: 'Ignored files',
-  },
-  ignoredExtensions: {
-    type: 'array',
-    description: 'Ignored extensions',
-  },
-} as Record<string, Options>
+export const options = {} as Record<string, Options>
 
 export const builder = (yargs: Argv) => {
   return yargs.options(options)
