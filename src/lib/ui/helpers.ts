283bd8220d59e7dcb7bcc9b13371fbe00bb0368d
Griffen Fargo <ghfargo@gmail.com>
2024-10-09 23:17:46 -0400

    Update UI helpers for improved display
    Refactor `LOGO` for a cleaner look and add `LOGO_SMALL` for compact display options. Enhance `USAGE_BANNER` by embedding the version in a highlighted format. Introduce `getCommandUsageHeader` to streamline command usage messages. These changes improve the visual appeal and clarity of the UI components.

--- src/lib/ui/helpers.ts
+++ src/lib/ui/helpers.ts
@@ -2,24 +2,36 @@ import chalk from 'chalk'
 import { loadConfig } from '../config/utils/loadConfig'
 
 export const isInteractive = (config: ReturnType<typeof loadConfig>) => {
-  return config?.mode === 'interactive' || !!config?.interactive 
+  return config?.mode === 'interactive' || !!config?.interactive
 }
 
 export const SEPERATOR = chalk.blue('─────────────')
 
 export const LOGO = chalk.green(
-  `┌────────────┐
-│┌─┐┌─┐┌─┐┌─┐│
-││  │ ││  │ ││
-│└─┘└─┘└─┘└─┘│
-└────────────┘`
+  `┌──────┐
+│┏┏┓┏┏┓│
+│┗┗┛┗┗┛│
+└──────┘`
 )
+
+export const LOGO_SMALL = chalk.green(
+  `┌────┐
+│coco│
+└────┘`)
 
 export const USAGE_BANNER = chalk.green(
   `${LOGO}
-v: ${process.env.npm_package_version}
+${chalk.bgGreen(`\xa0v${process.env.npm_package_version}\xa0`)}
 `
 )
+
+export const getCommandUsageHeader = (command: string) => {
+  return chalk.green(
+    `${USAGE_BANNER}\n${chalk.white('Command:')}\n\xa0\xa0\xa0\xa0\xa0 $0 ${chalk.greenBright(
+      command
+    )} [options]`
+  )
+}
 
 export const CONFIG_ALREADY_EXISTS = (path: string) => {
   return `coco config found in '${path}', do you want to override it?`
