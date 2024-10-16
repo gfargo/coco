76b9af6f51416cedc88ca68a1196d44b7ce8a1c6
Griffen Fargo <ghfargo@gmail.com>
2024-10-16 14:08:43 -0400

    Enhance UI helpers and improve diff handling
    Add new UI elements `DIVIDER` and `bannerWithHeader` for better visual structure. Introduce `severityColor` and `statusColor` functions for dynamic color coding based on severity and status. Improve handling of untracked files in `parseDefaultFileDiff` by reading directly from the filesystem, enhancing error handling for file read operations.

--- src/lib/ui/helpers.ts
+++ src/lib/ui/helpers.ts
@@ -6,6 +6,7 @@ export const isInteractive = (config: ReturnType<typeof loadConfig>) => {
 }
 
 export const SEPERATOR = chalk.blue('─────────────')
+export const DIVIDER = chalk.dim(`\\`)
 
 export const LOGO = chalk.green(
   `┌──────┐
@@ -17,7 +18,14 @@ export const LOGO = chalk.green(
 export const LOGO_SMALL = chalk.green(
   `┌────┐
 │coco│
+└────┘`
+)
+
+export const bannerWithHeader = (banner: string) => {
+  return chalk.green(`┌────┐
+│coco│ ${banner}
 └────┘`)
+}
 
 export const USAGE_BANNER = chalk.green(
   `${LOGO}
@@ -36,3 +44,35 @@ export const getCommandUsageHeader = (command: string) => {
 export const CONFIG_ALREADY_EXISTS = (path: string) => {
   return `existing config found in '${path}', do you want to override it?`
 }
+
+const severityColors = [
+  chalk.greenBright, // 1
+  chalk.green, // 2
+  chalk.cyan, // 3
+  chalk.yellowBright, // 4
+  chalk.yellow, // 5
+  chalk.bgYellow, // 6
+  chalk.red, // 7
+  chalk.redBright, // 8
+  chalk.bgRed, // 9
+  chalk.bgRedBright, // 10
+]
+
+export const severityColor = (severity: number) => {
+  return severityColors[Math.min(severity - 1, severityColors.length - 1)]
+}
+
+export const statusColor = (status: string) => {
+  switch (status) {
+    case 'completed':
+      return chalk.green
+    case 'skipped':
+      return chalk.yellow
+    case 'omitted':
+      return chalk.red
+    default:
+      return chalk.blue
+  }
+}
+
+export const hotKey = (key: string) => chalk.dim(`(${key})`)
