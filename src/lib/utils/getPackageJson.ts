843df000359345fe0052e771940776248fa6642c
Griffen Fargo <ghfargo@gmail.com>
2024-10-25 19:03:24 -0400

    Refactor version handling
    Replace `version` with `BUILD_VERSION` for better consistency and maintainability. Remove `getPackageJson` utility as it's no longer needed. Update `schema.ts` to include `BUILD_VERSION` directly, ensuring auto-generated comments are clear and informative. Simplify imports and improve code readability.
    
    Part of #260

--- src/lib/utils/getPackageJson.ts
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-//Reach the package.json file
-export function getPackageJson() {
-  const path = `${process.cwd()}/package.json`;
-  const packageData = JSON.parse(fs.readFileSync(path, "utf8"));
-  return packageData;
-}
